3c64b832a9235f63bbfb3b7b15edae4c90376808
Azeem Brown <azeembrown717@gmail.com>
2025-10-07 14:36:52 -0400

    add button start

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,22 +1,22 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react'
+import { IonFabButton, IonIcon } from '@ionic/react';
+import { add } from 'ionicons/icons';
+import * as ExploreContainer from '../components/ExploreContainer'
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
-  return (
+return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Tour Of Heroes</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <IonFabButton>
+          <IonIcon icon={add}></IonIcon>
+        </IonFabButton>
+        <IonContent></IonContent>
       </IonContent>
     </IonPage>
   );
