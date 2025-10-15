200345b3a9bf1633e3031a37049cf7cb466f7a75
Azeem Brown <azeembrown717@gmail.com>
2025-10-15 16:58:41 -0400

    Experiment

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -7,23 +7,31 @@ import IHero from '../Interfaces/Iheroes.interface';
 const Tab2: React.FC = () => {
 
   const [data, setData] = React.useState<IHero[]>([]);
+  const [heroData, setHeroData] = React.useState<IHero | undefined>();
+
   heroes().then(heroes => setData(heroes));
+
+  const getHero = (id: number) => {
+     setHeroData(data.find(hero => hero.id === id));
+  }
 
 return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tour Of Heroes</IonTitle>
+          <IonTitle>{heroData?.name} is from the city: {heroData?.cityId}</IonTitle>
         </IonToolbar>
       </IonHeader>
     <IonContent fullscreen>
-      {data.map(hero => (
-        <div key={hero.id}>
-          <IonButton color={"success"} shape='round'>
-            <p>{hero.name}</p>
-          </IonButton>
-        </div>
-      ))}
+      <div className="ailgn-items-center justify-content-center ion-padding">
+        {data.map(hero => (
+          <div key={hero.id} className="text-center">
+            <IonButton color={"success"} shape='round' onClick={() => getHero(hero.id)}>
+              <p>{hero.name}</p>
+            </IonButton>
+          </div>
+        ))}        
+      </div>
       </IonContent>
     </IonPage>
   );
